e5942e85a5fcd5a15508502db77a4c88ce45a524
MaeraContaret <maera.contaret@outlook.fr>
2021-12-25 16:45:37 +0100

    Issue 1 : modale fermée

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelector('.close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,9 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+close.addEventListener('click' , () => {
+  modalbg.style.display = "none";
+})
 
 
